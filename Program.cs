7de19939e19890aafee5836cbc335d56603bd28c
ElNeonnn <jndrsn.08@gmail.com>
2026-04-03 07:01:39 -0300

    iniciando a criação do projeto

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Hosting;
+using NetCord.Hosting.Gateway;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services
+    .AddDiscordGateway();
+
+var bot = builder.Build();
+
+await bot.RunAsync();
